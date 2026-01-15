5709e75c873f768e680a798293d078e49970c999
Drafter Furniture <reddamaskus@gmail.com>
2026-01-15 12:24:55 +0700

    Create logout.js

---
+++ functions/api/logout.js
@@ -0,0 +1,15 @@
+import { ok, parseCookies } from "./_utils.js";
+
+export async function onRequestPost({ request, env }){
+  const cookies = parseCookies(request);
+  const sid = cookies[env.COOKIE_NAME || "bom_session"];
+  if(sid){
+    await env.BOM_DB.prepare("DELETE FROM sessions WHERE id=?").bind(sid).run();
+  }
+  return new Response(JSON.stringify({ ok:true }), {
+    headers:{
+      "content-type":"application/json; charset=utf-8",
+      "set-cookie": `${env.COOKIE_NAME || "bom_session"}=; Path=/; HttpOnly; SameSite=Lax; Max-Age=0`
+    }
+  });
+}
